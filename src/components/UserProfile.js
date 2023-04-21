55953e7d4f26b7139eb37a166ffe84282990dfc6
Corey Donenfeld <corey@studiosimpati.co>
2023-04-21 15:39:13 -0400

    Styling for login + user profile

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -11,12 +11,26 @@ class UserProfile extends Component {
     render() {
         return (
             <div>
-                <h1>User Profile</h1>
+                <div className="container">
+                    <h1 className="heading-2 page-title">User Profile</h1>
 
-                <div>Username: {this.props.userName}</div>
-                <div>Member Since: {this.props.memberSince}</div>
-                <br />
-                <Link to="/">Return to Home</Link>
+                    <div className="modules grid">
+                        <section className="module user-profile">
+                            <ul className="grid">
+                                <li>
+                                    <h2 className="eyebrow">Username</h2>
+                                    <p class="heading-5">{this.props.userName}</p>
+                                </li>
+                                <li>
+                                    <h2 className="eyebrow">Member Since</h2>
+                                    <p class="heading-5">{this.props.memberSince}</p>
+                                </li>
+                            </ul>
+                        </section>
+                    </div>
+                    
+                    <Link to="/" className="btn secondary">Return to Home</Link>  
+                </div>
             </div>
         );
     }
